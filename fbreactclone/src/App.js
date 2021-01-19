9af7e6db9d32ba02817c892e3962a33a202c5eed
jabcodes23 <jabwrites23@gmail.com>
2021-01-19 15:45:55 -0500

    Added some base code

---
+++ fbreactclone/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Let's build</h1>
+
+      {/* Header */}
+
+      {/* App body */}
+        {/* Sidebar */}
+        {/* Feed */}
+        {/* Widgets */}
+    </div>
+  );
+}
+
+export default App;
---
+++ fbreactclone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA5I8o-uWFY_KXHO8N43N-vXvQ23qal5wk",
+    authDomain: "fbreactclone-6245a.firebaseapp.com",
+    projectId: "fbreactclone-6245a",
+    storageBucket: "fbreactclone-6245a.appspot.com",
+    messagingSenderId: "489234956154",
+    appId: "1:489234956154:web:d22286a05d51c0a1e2e0c3",
+    measurementId: "G-WSPKHDEVD4"
+};
